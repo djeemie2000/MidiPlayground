e6cd2bb79237d722e6787e034597d7794f24cbf6
djeemie2000 <peter.jamee@telenet.be>
2014-12-29 18:23:53 +0100

    Shift register outputs using class

--- ShiftRegister8Leds/ShiftRegister8Leds.ino
+++ ShiftRegister8Leds/ShiftRegister8Leds.ino
@@ -1,12 +1,7 @@
 //**************************************************************//
-//  Name    : shiftOutCode, Hello World                                
-//  Author  : Carlyn Maw,Tom Igoe, David A. Mellis 
-//  Date    : 25 Oct, 2006    
-//  Modified: 23 Mar 2010                                 
-//  Version : 2.0                                             
-//  Notes   : Code for using a 74HC595 Shift Register           //
-//          : to count from 0 to 255         ;                   
 //****************************************************************
+
+#include "ShiftRegisterOutputs.h"
 
 //Pin connected to ST_CP of 74HC595
 int latchPin = 8;
@@ -15,28 +10,35 @@ int clockPin = 12;
 ////Pin connected to DS of 74HC595
 int dataPin = 11;
 
+CShiftRegisterOutputs Outputs;
 
 
-void setup() {
+void setup() 
+{
+  
+  Outputs.Begin(latchPin, clockPin, dataPin);
   //set pins to output so you can control the shift register
-  pinMode(latchPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
+//  pinMode(latchPin, OUTPUT);
+//  pinMode(clockPin, OUTPUT);
+//  pinMode(dataPin, OUTPUT);
 }
 
-void loop() {
-  // count from 0 to 255 and display the number 
-  // on the LEDs
-  for (int numberToDisplay = 0; numberToDisplay < 8; numberToDisplay++) {
-    // take the latchPin low so 
-    // the LEDs don't change while you're sending in bits:
-    digitalWrite(latchPin, LOW);
-    // shift out the bits:
-    shiftOut(dataPin, clockPin, MSBFIRST, 1<<numberToDisplay);  
-
-    //take the latch pin high so the LEDs will light up:
-    digitalWrite(latchPin, HIGH);
+void loop() 
+{
+  for (int numberToDisplay = 0; numberToDisplay < 8; numberToDisplay++) 
+  {
+    Outputs.Set(numberToDisplay, true);
+    Outputs.Write();
     // pause before next value:
     delay(500);
   }
+  
+  for (int numberToDisplay = 0; numberToDisplay < 8; numberToDisplay++) 
+  {
+    Outputs.Set(numberToDisplay, false);
+    Outputs.Write();
+    // pause before next value:
+    delay(500);
+  }
+
 }
