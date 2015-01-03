635426dbeee57c02a5e3aa42289cc9a62942dc6e
djeemie2000 <peter.jamee@telenet.be>
2015-01-03 11:37:38 +0100

    Analog mux/demux (4051 mux/demux)

---
+++ DemuxerAnalogRead/DemuxerAnalogRead.ino
@@ -0,0 +1,56 @@
+
+
+/*
+
+Reading multiple analog input through a 4051 mux/demux
+
+*/
+
+const int AnalogReadPin = A0;// common analog read pin
+
+const int SelectPin0 = 2;
+const int SelectPin1 = 3;
+const int SelectPin2 = 4;
+
+void setup()
+{
+  pinMode(AnalogReadPin, INPUT);
+  
+  pinMode(SelectPin0, OUTPUT);
+  pinMode(SelectPin1, OUTPUT);
+  pinMode(SelectPin2, OUTPUT); 
+  
+  Serial.begin(9600);
+  
+  Serial.println("Starting....");
+}
+
+int AnalogRead(int Input)
+{
+  
+  // select input
+  digitalWrite(SelectPin0, (Input&0x01) ? HIGH : LOW);
+  digitalWrite(SelectPin1, (Input&0x02) ? HIGH : LOW);
+  digitalWrite(SelectPin2, (Input&0x04) ? HIGH : LOW);
+  
+  int Value = analogRead(AnalogReadPin);
+  return Value;
+}
+
+void loop()
+{
+   Serial.println("Measuring...");
+   
+   for(int Input = 0; Input<2; ++Input)
+   {
+     int Value = AnalogRead(Input);
+     Serial.print(Input);
+     Serial.print(" : ");
+     Serial.println(Value);
+     
+//     delay(100);
+   }
+  
+  delay(1000);
+}
+
