718b1322f2661ca1a3ed213603a0d560cc7a869a
djeemie2000 <peter.jamee@telenet.be>
2015-01-03 19:23:17 +0100

    Reworked Rotary encoder test.

---
+++ RotaryTest/RotaryEncoder.cpp
@@ -0,0 +1,81 @@
+#include "RotaryEncoder.h"
+#include <Arduino.h>
+
+CRotaryEncoder::CRotaryEncoder()
+ : m_PinA(-1)
+ , m_PinB(-1)
+ , m_RotaryHistory(0)
+ , m_Position(0)
+ , m_ChangeCount(0)
+{
+}
+
+void CRotaryEncoder::Begin(int PinA, int PinB)
+{
+    m_PinA = PinA;
+    m_PinB = PinB;
+
+    pinMode(m_PinA, INPUT);
+    pinMode(m_PinB, INPUT);
+}
+
+void CRotaryEncoder::Read()
+{
+    unsigned int NewRotaryValue = 2*digitalRead(m_PinA) + digitalRead(m_PinB);
+    unsigned int PrevRotaryValue = m_RotaryHistory%4;
+    if(NewRotaryValue != PrevRotaryValue)
+    {
+      // changed
+      ++m_ChangeCount;
+
+      if(!NewRotaryValue)
+      {
+        // new pos => increase or decrease?
+        if(PrevRotaryValue == 2)
+        {
+          ++m_Position;
+        }
+        else if(PrevRotaryValue == 1)
+        {
+          --m_Position;
+        }
+        else if(PrevRotaryValue == 3)
+        {
+          // skipped one
+          unsigned int PrevPrevRotaryValue = (m_RotaryHistory/4)%4;
+          if(PrevPrevRotaryValue == 1)
+          {
+            ++m_Position;
+          }
+          else if(PrevPrevRotaryValue == 2)
+          {
+            --m_Position;
+          }
+        }
+      }
+
+      m_RotaryHistory = 4*m_RotaryHistory + NewRotaryValue;
+    }
+}
+
+int CRotaryEncoder::GetPosition() const
+{
+    return m_Position;
+}
+
+void CRotaryEncoder::Reset()
+{
+    m_Position = 0;
+    m_RotaryHistory = 0;
+    m_ChangeCount = 0;
+}
+
+int CRotaryEncoder::GetChangeCount() const
+{
+    return m_ChangeCount;
+}
+
+unsigned int CRotaryEncoder::GetRotaryHistory() const
+{
+    return m_ChangeCount;
+}
--- RotaryTest/RotaryTest.ino
+++ RotaryTest/RotaryTest.ino
@@ -1,81 +1,41 @@
-
+#include "RotaryEncoder.h"
 
 const int PinA = 2;
 const int PinB = 3;
 
-unsigned int RotaryValue;
-int ChangeCount;
-int Position;
+CRotaryEncoder Encoder;
+int PrevPosition;
 
 void setup()
 {
-  pinMode(PinA, INPUT);
-  pinMode(PinB, INPUT);
-  
-  RotaryValue = 0;
-  ChangeCount = 0;
-  Position = 0;
-  
+  Encoder.Begin(PinA, PinB);
   Serial.begin(115200);
+  PrevPosition = 0;
 }
 
 void PrintState()
 {  
-  Serial.print((RotaryValue/4)%4);
+  Serial.print((Encoder.GetRotaryHistory()/4)%4);
   Serial.print(" ");
-  Serial.print(RotaryValue%4);
+  Serial.print(Encoder.GetRotaryHistory()%4);
   Serial.print(" #");
-  Serial.print(ChangeCount);
+  Serial.print(Encoder.GetChangeCount());
   Serial.print(" -> ");
-  Serial.println(Position);
+  Serial.println(Encoder.GetPosition());
 }
 
 void loop()
 {
-  //
-  unsigned int NewRotaryValue = 2*digitalRead(PinA) + digitalRead(PinB);
-  unsigned int PrevRotaryValue = RotaryValue%4;
-  if(NewRotaryValue != PrevRotaryValue)
+  Encoder.Read();
+
+  if(PrevPosition != Encoder.GetChangeCount())
   {
-    // change
-    ++ChangeCount;
-
-    if(!NewRotaryValue)
-    {
-      // new pos => increase or decrease?
-      if(PrevRotaryValue == 2)
-      {
-        ++Position;
-      }      
-      else if(PrevRotaryValue == 1)
-      {
-        --Position;
-      }
-      else if(PrevRotaryValue == 3)
-      {
-        // skipped one
-        unsigned int PrevPrevRotaryValue = (RotaryValue/4)%4;
-        if(PrevPrevRotaryValue == 1)
-        {
-          ++Position;
-        }      
-        else if(PrevPrevRotaryValue == 2)
-        {
-          --Position;
-        }
-      }      
-    }
-
-    RotaryValue = 4*RotaryValue + NewRotaryValue;    
-    
-    if(!NewRotaryValue)
-    {
-      PrintState();
-    }
+    PrevPosition = Encoder.GetChangeCount();
+    PrintState();
   }
-  
   delay(5);//will this prevent proper operation?
   // 10 mSec will sometimes cause skips
   // 5 mSec or below will give good performance
+  // read more often => better performance
 }
 
