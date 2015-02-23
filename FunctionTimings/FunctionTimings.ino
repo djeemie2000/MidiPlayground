bec2a27e65cf47d8d80cf48e65f7b6aa6f199c02
djeemie2000 <peter.jamee@telenet.be>
2015-02-23 09:53:56 +0100

    Analog Read / Write timing with optional speedup

--- FunctionTimings/FunctionTimings.ino
+++ FunctionTimings/FunctionTimings.ino
@@ -6,6 +6,8 @@ Results on Uno:
 
 AnalogRead x 10000 = 1120 msec => 0.11 mSec
 
+AnalogWrite x 10000 = 120 msec => 0.012 mSec
+
 DigitalRead x 10000 = 50 msec => 5 uSec
 
 DigitalWrite x 10000 = 53 msec => 5 uSec
@@ -18,12 +20,26 @@ Serial.print(int) x 100 = 163 mSec @9600 Baud => 1.63 mSec
 const int DigitalReadPin = 5;
 const int DigitalWritePin = 6;
 const int AnalogReadPin = A0;
+const int AnalogWritePin = A1;
+
+void setupFastAnalogRead()
+{
+  // bits [2:0] of ADCSRA register determine clock divider
+  // by default it is 128 ~ 111
+  // we can set it to 32 ~ 101
+  // we can set it to 16 ~ 100
+  // we can set it to 8  ~ 011
+  ADCSRA &= (0xF8 | 0x04);
+}
 
 void setup()
 {
   pinMode(DigitalReadPin, INPUT_PULLUP);
   pinMode(DigitalWritePin, OUTPUT);
-  pinMode(AnalogReadPin, OUTPUT);
+  pinMode(AnalogReadPin, INPUT);
+  pinMode(AnalogWritePin, OUTPUT);
+ 
+  //setupFastAnalogRead();
   
   Serial.begin(115200);
 }
@@ -82,6 +98,25 @@ void TimingAnalogRead()
   Serial.println(" msec");
 }
 
+void TimingAnalogWrite()
+{
+  const int NumRepeats = 10000;
+  unsigned long Before = millis();
+  for(int Repeat = 0; Repeat<NumRepeats; ++Repeat)
+  {
+     analogWrite(AnalogWritePin, Repeat%1024); 
+  }
+  unsigned long After = millis();
+  unsigned long Duration = After-Before;
+  
+  Serial.print("AnalogWrite x ");
+  Serial.print(NumRepeats);
+  Serial.print(" = ");
+  Serial.print(Duration);
+  Serial.println(" msec");
+}
+
+
 void TimingSerial()
 {
   const int NumRepeats = 100;
@@ -104,9 +139,10 @@ void TimingSerial()
 
 void loop()
 {
-  TimingSerial();
-  TimingDigitalRead();
-  TimingDigitalWrite();
+  //TimingSerial();
+  //TimingDigitalRead();
+  //TimingDigitalWrite();
   TimingAnalogRead();
+  TimingAnalogWrite();
 }
 
