1e87aa141f39ac8aefbef042ff773a757999063b
djeemie2000 <peter.jamee@telenet.be>
2015-08-30 09:34:44 +0200

    Due mcp4822 test:  cleanup

--- DueMCP4822Test/DueMCP4822Test.ino
+++ DueMCP4822Test/DueMCP4822Test.ino
@@ -9,24 +9,23 @@ void setup()
   Serial.begin(115200);
   Serial.println("DUE MCP4822 test 2...");
 
-  //pinMode(PIN_CS, OUTPUT);
-  SPI.begin(PIN_CS);//can be 4, 10 or ??
-  //SPI.setClockDivider(SPI_CLOCK_DIV2); //AVR: defult is 4 so 16MHz/4 = 4MHz
-  SPI.setClockDivider(PIN_CS, 4);//DUE: default is 21 corresponding to 4MHz as in AVR
+  mcp48_begin();
 }
 
-//assuming single channel, gain=2
+void mcp48_begin()
+{
+  SPI.begin(PIN_CS);//can be 4, 10 or ??
+  SPI.setClockDivider(PIN_CS, 4);//DUE: default is 21 corresponding to 4MHz as in AVR  
+}
+
 void setOutput(unsigned int val)
 {
+  //assuming single channel, gain=2
   byte lowByte = val & 0xff;
   byte highByte = ((val >> 8) & 0xff) | 0x10;
 
-//  PORTB &= 0xfb;
-  //digitalWrite(PIN_CS, LOW);
   SPI.transfer(PIN_CS, highByte, SPI_CONTINUE);
   SPI.transfer(PIN_CS, lowByte);
-  //digitalWrite(PIN_CS, HIGH);
-//  PORTB |= 0x4;
 }
 
 void setOutput(byte channel, byte gain, byte shutdown, unsigned int val)
@@ -34,12 +33,8 @@ void setOutput(byte channel, byte gain, byte shutdown, unsigned int val)
   byte lowByte = val & 0xff;
   byte highByte = ((val >> 8) & 0xff) | channel << 7 | gain << 5 | shutdown << 4;
 
-  //PORTB &= 0xfb;
-  //digitalWrite(PIN_CS, LOW);
   SPI.transfer(PIN_CS, highByte, SPI_CONTINUE);
   SPI.transfer(PIN_CS, lowByte);
-  //digitalWrite(PIN_CS, HIGH);
-  //PORTB |= 0x4;
 }
 
 void TestAccuracy()
@@ -140,10 +135,10 @@ void TestSquare()
 
 void loop()
 {
-    TestAccuracy();
-    TestSpeed();
+  //  TestAccuracy();
+  //  TestSpeed();
   //  delay(500);
-  //TestSaw();
+  TestSaw();
   //TestSquare();
 }
 
