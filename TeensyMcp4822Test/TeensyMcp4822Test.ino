0b4f9c6252a6c3e8f6b9fc0d3bc88a6db34e4497
djeemie2000 <peter.jamee@telenet.be>
2015-09-28 20:20:22 +0200

    Teensy mcp4822 test: saw, saw with timer.

--- TeensyMcp4822Test/TeensyMcp4822Test.ino
+++ TeensyMcp4822Test/TeensyMcp4822Test.ino
@@ -1,6 +1,7 @@
 #include <SPI.h>
 #include "spi4teensy3.h"
 #include "TeensyMcpDac.h"
+#include "TimerOne.h"
 
 const int AnalogInPin = A0;
 
@@ -39,6 +40,39 @@ void TestSpeed()
   
 }
 
+void TestSaw()
+{
+  for(int Value = 0; Value<4096; Value+=1)
+  {
+    mcp48_setOutput(Value);
+  }
+}
+
+void WriteDac()
+{
+  static int DacValue = 0;
+  DacValue = (DacValue + 16)%4096;
+  mcp48_setOutput(DacValue);
+}
+
+void TestSawInterrupt()
+{
+  const int SamplingFrequency = 40000;
+  const unsigned long PeriodMicroSeconds = 1000000ul / SamplingFrequency;
+  Serial.print("Fs=");
+  Serial.print(SamplingFrequency);
+  Serial.print(" Period=");
+  Serial.print(PeriodMicroSeconds);
+  Serial.println(" uSec");
+  
+  Serial.println("Starting..");
+  Timer1.initialize(PeriodMicroSeconds);
+  Timer1.attachInterrupt(WriteDac);
+  delay(5000);
+  Timer1.detachInterrupt();
+  delay(1000);
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -54,8 +88,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  TestSpeed();
-  TestAccuracy();
-  delay(2000);
+  //TestSpeed();
+  //TestAccuracy();
+  //delay(2000);
+  //TestSaw();
+  TestSawInterrupt();
 }
 
