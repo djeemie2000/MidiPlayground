b8516ac9a3ed5da9678943c3c94a1b26ec105eb9
djeemie2000 <peter.jamee@telenet.be>
2017-01-20 20:12:20 +0100

    BitNoise: members to locals => cleaner and faster

--- BitNoise/BitNoise.cpp
+++ BitNoise/BitNoise.cpp
@@ -6,14 +6,13 @@ CBitNoise::CBitNoise()
 }
 int CBitNoise::Generate()
 {
-    b31 = (reg & (1L << 31)) >> 31;
-    b29 = (reg & (1L << 29)) >> 29;
-    b25 = (reg & (1L << 25)) >> 25;
-    b24 = (reg & (1L << 24)) >> 24;
-    lobit = b31 ^ b29 ^ b25 ^ b24;
-    newr = (reg << 1) | lobit;
-    reg = newr;
-
+    unsigned char b31 = (reg & (1L << 31)) >> 31;
+    unsigned char b29 = (reg & (1L << 29)) >> 29;
+    unsigned char b25 = (reg & (1L << 25)) >> 25;
+    unsigned char b24 = (reg & (1L << 24)) >> 24;
+    unsigned char lobit = b31 ^ b29 ^ b25 ^ b24;
+    reg = (reg << 1) | lobit;
+    
     return reg & 1;
 };
 
--- BitNoise/BitNoise.h
+++ BitNoise/BitNoise.h
@@ -7,10 +7,8 @@ class CBitNoise
 public:
     CBitNoise();
     int Generate();
+
 private:
     unsigned long int reg;
-    unsigned long int newr;
-    unsigned char lobit;
-    unsigned char b31, b29, b25, b24;
 };
 
