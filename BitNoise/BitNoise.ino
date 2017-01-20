15606447462632de3c79972f0174d3afa7434f50
djeemie2000 <peter.jamee@telenet.be>
2017-01-20 20:02:26 +0100

    BinNoise: timings

--- BitNoise/BitNoise.ino
+++ BitNoise/BitNoise.ino
@@ -5,6 +5,7 @@ const int AnalogInPin1 = A0;
 const int NoiseOutPin = 4;
 
 unsigned long g_DebugCounter;
+unsigned long g_Millis;
 
 void setupFastAnalogRead()
 {
@@ -68,25 +69,122 @@ void setup()
     g_NoiseGen[Gen].Begin(NoiseOutPin+Gen);
     g_NoiseGen[Gen].SetColor(Gen+1);
   }
+
+  TimingBitNoise();
+  TimingUpdate();
+  TimingDigitalWrite();
+  TimingAnalogRead();
 }
 
 void Debug()
 {
   // for debugging / timing purposes
   ++g_DebugCounter;
-  unsigned long Interval = 1024*16;
+  unsigned long Interval = 10000;//1024*16;
   if(Interval<g_DebugCounter)
   {
+    unsigned long currMillis = millis();
     Serial.println();
-    Serial.println(millis());
+    Serial.println(currMillis-g_Millis);
     Serial.println(g_DebugCounter);
     g_DebugCounter = 0;
+    g_Millis = currMillis;
   }
 }
+
+void TimingBitNoise()
+{
+  Serial.println("TimingBitNoise");
+
+  unsigned long Before = millis();
+  
+  const int NumRepeats = 10000;
+  int Dummy = 0;
+  for(int Repeat = 0; Repeat<10000; ++Repeat)
+  {
+    Dummy += g_BitNoise.Generate();
+  }
+
+  unsigned long After = millis();
+  
+  Serial.print("Repeat ");
+  Serial.print(NumRepeats);
+  Serial.print(" -> ");
+  Serial.println(After-Before);
+  Serial.println(Dummy);
+}
+
+void TimingDigitalWrite()
+{
+  Serial.println("TimingDigitalWrite");
+
+  unsigned long Before = millis();
+  
+  const int NumRepeats = 10000;
+  for(int Repeat = 0; Repeat<10000; ++Repeat)
+  {
+    digitalWrite(NoiseOutPin, Repeat&1);
+  }
+
+  unsigned long After = millis();
+  
+  Serial.print("Repeat ");
+  Serial.print(NumRepeats);
+  Serial.print(" -> ");
+  Serial.println(After-Before);
+}
+
+void TimingAnalogRead()
+{
+  Serial.println("TimingAnalogRead");
+
+  unsigned long Before = millis();
+  
+  const int NumRepeats = 10000;
+  int Dummy = 0;
+  for(int Repeat = 0; Repeat<10000; ++Repeat)
+  {
+    Dummy += analogRead(AnalogInPin1);
+  }
+
+  unsigned long After = millis();
+  
+  Serial.print("Repeat ");
+  Serial.print(NumRepeats);
+  Serial.print(" -> ");
+  Serial.println(After-Before);
+  Serial.println(Dummy);
+}
+
+
+void TimingUpdate()
+{
+  Serial.println("TimingUpdate");
+
+  unsigned long Before = millis();
+  
+  const int NumRepeats = 10000;
+  for(int Repeat = 0; Repeat<10000; ++Repeat)
+  {
+    g_NoiseGen[0].Update(Repeat);
+    g_NoiseGen[1].Update(Repeat);
+    g_NoiseGen[2].Update(Repeat);
+    g_NoiseGen[3].Update(Repeat);
+  }
+
+  unsigned long After = millis();
+  
+  Serial.print("Repeat ");
+  Serial.print(NumRepeats);
+  Serial.print(" -> ");
+  Serial.println(After-Before);
+}
+
 
 void loop() 
 {
   int GlobalColor = analogRead(AnalogInPin1);
+  GlobalColor = 0;//debug!!!!
 
   // generate next bit
   int CurrentBit = g_BitNoise.Generate();
