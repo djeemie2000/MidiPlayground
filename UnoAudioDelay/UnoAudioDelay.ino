a47f7e81f435843d8656b1726a916f25ec3b5264
djeemie2000 <peter.jamee@telenet.be>
2017-11-19 08:37:57 +0100

    Uno audio delay: fixed sampling rate, delay from CV input, smooth CV.

--- UnoAudioDelay/UnoAudioDelay.ino
+++ UnoAudioDelay/UnoAudioDelay.ino
@@ -21,6 +21,8 @@ isl::CDelayLine<unsigned int, DelayLineCapacity> g_DelayLine;
 unsigned int g_DebugCounter;
 unsigned long g_Millis;
 
+int g_DelayTime;
+
 void setupFastAnalogRead()
 {
   // bits [2:0] of ADCSRA register determine clock divider
@@ -50,6 +52,8 @@ void setup()
 {
   g_DebugCounter = 0;
   g_Millis = 0;
+
+  g_DelayTime = 0;
   
   pinMode(AnalogPinAudioIn, INPUT);
   pinMode(AnalogPinSleepIn, INPUT);
@@ -66,7 +70,7 @@ void setup()
 void Debug()
 {
   ++g_DebugCounter;
-  if(g_DebugCounter>20000)
+  if(g_DebugCounter>=DelayLineCapacity)
   {
     Serial.print(g_DebugCounter);
     Serial.print(" ");
@@ -75,32 +79,48 @@ void Debug()
     unsigned long Elapsed = g_Millis - PrevMillis;
     Serial.println(Elapsed);
 
+    float FreqHz = float(DelayLineCapacity)/Elapsed;
+    Serial.println(FreqHz);
+
     g_DebugCounter = 0;
   }  
 }
 
 void loop()
 {
+  // [0,1024[ -> [0,1024+512[  
+  int DelayTime = 3*analogRead(AnalogPinSleepIn)/2;//DelayLineCapacity-2;
+  
+  // smooth delay time
+  if(g_DelayTime)
+  {
+    g_DelayTime = (DelayTime+7*g_DelayTime)>>3; 
+  }
+  else
+  {
+    g_DelayTime = DelayTime;
+  }
+
 #ifdef DELAY_LINE_8BIT
   //10 bits input 8 bits delay line
   byte Value = analogRead(AnalogPinAudioIn)>>2;  
   g_DelayLine.Write(Value);
   // 8 bits delay line 12 bits output
-  mcp48dac::SetOutput(g_DelayLine.Read(DelayLineCapacity-2)<<4, mcp48dac::Channel_A, mcp48dac::Gain_x1);
+  mcp48dac::SetOutput(g_DelayLine.Read(g_DelayTime)<<4, mcp48dac::Channel_A, mcp48dac::Gain_x1);
 #else
   //10 bits input 12 bits output
   unsigned int Value = analogRead(AnalogPinAudioIn)<<2;  
   g_DelayLine.Write(Value);
-  mcp48dac::SetOutput(g_DelayLine.Read(DelayLineCapacity-2), mcp48dac::Channel_A, mcp48dac::Gain_x1);
+  mcp48dac::SetOutput(g_DelayLine.Read(g_DelayTime), mcp48dac::Channel_A, mcp48dac::Gain_x1);
 #endif
 
   // read sleep time from 2nd AnalogIn 
-  int SleepTime = analogRead(AnalogPinSleepIn);
+  int SleepTime = 96;//128;//analogRead(AnalogPinSleepIn);
   delayMicroseconds(SleepTime);
 
   // TODO read actual delay from analog in
   // TODO read sleep time and delay alternating -> g_
  
-  // Debug();  
+  //Debug();  
 }
 
